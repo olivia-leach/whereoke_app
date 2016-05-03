b41e400b6b9d221fc42b8b0483ee49a41e5a1ecd
olivia-leach <olivia.i.m.leach@gmail.com>
2016-05-03 16:04:41 -0400

    Remove google maps API alerts

--- assets/scripts/app/google_map_add.js
+++ assets/scripts/app/google_map_add.js
@@ -51,10 +51,10 @@ const mapGeocoder = function() {
           });
 
         } else {
-          alert("No results found");
+          console.log("No results found");
         }
       } else {
-        alert("Geocode was not successful for the following reason: " + status);
+        console.log("Geocode was not successful for the following reason: " + status);
       }
     });
   }
